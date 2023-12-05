304a73434ae33c150aee41b5f2674d63a8ea9b43
Anete <anetempereira@gmail.com>
2023-12-05 05:49:18 +0000

    added semantic search for circuits but having problems with queries

---
+++ Milestone3/get_embeddings.py
@@ -0,0 +1,31 @@
+import sys
+import json
+from sentence_transformers import SentenceTransformer
+
+# example usage: cat ../f1db_json/circuits.json | python3 get_embeddings.py > semantic_files/circuits_semantic.json
+
+# Load the SentenceTransformer model
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def get_embedding(text):
+    # The model.encode() method already returns a list of floats
+    return model.encode(text, convert_to_tensor=False).tolist()
+
+if __name__ == "__main__":
+    # Read JSON from STDIN
+    data = json.load(sys.stdin)
+
+    # Update each document in the JSON data
+    for document in data:
+        # Extract fields if they exist, otherwise default to empty strings
+        circuitRef = document.get("circuitRef", "")
+        name = document.get("name", "")
+        location = document.get("location", "")
+        country = document.get("country", "")
+        circuit_bio = document.get("circuit_bio", "")
+
+        combined_text = circuitRef + " " + name + " " + location + " " + country + " " + circuit_bio
+        document["vector"] = get_embedding(combined_text)
+
+    # Output updated JSON to STDOUT
+    json.dump(data, sys.stdout, indent=4, ensure_ascii=False)
